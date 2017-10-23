4e174dd732b6a6a2646c377c9306994961127c5c
Andrew Jones <andrewjones232@gmail.com>
2017-10-23 16:50:26 -0400

    init

---
+++ PersonalSite/Models/EmptyClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace PersonalSite.Models
+{
+    public class Post
+    {
+        public Post()
+        {
+        }
+
+        public int PostId
+        {
+            get;
+            set;
+        }
+
+        public string Title
+        {
+            get;
+            set;
+        }
+
+        public string Body
+        {
+            get;
+            set;
+        }
+
+        public string UrlSlug
+        {
+            get;
+            set;
+        }
+
+        public DateTime PostedOn
+        {
+            get;
+            set;
+        }
+
+
+    }
+}
