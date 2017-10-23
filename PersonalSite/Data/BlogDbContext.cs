0894f014d01a5e5af44ac4725b75496d0fa0a479
Andrew Jones <andrewjones232@gmail.com>
2017-10-23 16:56:57 -0400

    Manually create BlogDbContext

---
+++ PersonalSite/Data/BlogDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using PersonalSite.Models;
+namespace PersonalSite.Data
+{
+    public class BlogDbContext : DbContext
+    {
+        
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=blogging.db");
+        }
+    }
+}
